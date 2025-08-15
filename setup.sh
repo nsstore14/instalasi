bb51cda9eb51092ab9ce65336668244af5be1124
nsstore14 <nursidik716@gmail.com>
2025-08-15 18:09:42 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -759,17 +759,17 @@ function ins_Fail2ban(){
 function ins_epro(){
 clear
 print_install "Menginstall WebSocket Python"
-wget -O /usr/bin/ws "${REPO}files/ws" >/dev/null 2>&1
+wget -O /usr/bin/ws-stunnel "${REPO}files/ws-stunnel" >/dev/null 2>&1
 wget -O /usr/bin/tun.conf "${REPO}config/tun.conf" >/dev/null 2>&1
-wget -O /etc/systemd/system/ws.service "${REPO}files/ws.service" >/dev/null 2>&1
-chmod +x /etc/systemd/system/ws.service
-chmod +x /usr/bin/ws
+wget -O /etc/systemd/system/ws-stunnel.service "${REPO}files/ws-stunnel.service" >/dev/null 2>&1
+chmod +x /etc/systemd/system/ws-stunnel.service
+chmod +x /usr/bin/ws-stunnel
 chmod 644 /usr/bin/tun.conf
-systemctl disable ws
-systemctl stop ws
-systemctl enable ws
-systemctl start ws
-systemctl restart ws
+systemctl disable ws-stunnel
+systemctl stop ws-stunnel
+systemctl enable ws-stunnel
+systemctl start ws-stunnel
+systemctl restart ws-stunnel
 wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
 wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
 wget -O /usr/sbin/ftvpn "${REPO}files/ftvpn" >/dev/null 2>&1
